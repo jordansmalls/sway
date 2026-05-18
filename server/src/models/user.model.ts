e94d1f4f4d2dd551de6003a3d37f53c31eaf55de
Jordan Smalls <smalls.jordan@gmail.com>
2026-05-18 15:35:28 -0400

    fix: update user model to properly handle password hashing when creating new users

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -68,11 +68,10 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
 };
 
 // encrypt password using bcrypt
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    next();
+    return
   }
-
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
