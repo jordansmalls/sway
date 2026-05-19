dce346b5c90e8f15860ef91b2e1264c687d04add
Jordan Smalls <smalls.jordan@gmail.com>
2026-05-19 16:57:52 -0400

    add: add model/schema for requests

---
+++ server/src/models/request.model.ts
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+
+const requestSchema = new Schema({
+    roomId: {
+        type: Schema.Types.ObjectId,
+        ref: "Room",
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "playing", "played", "rejected"],
+        default: "pending",
+    },
+    votes: {
+        type: Number,
+        default: 1,
+        min: 1,
+    },
+    playedAt: {
+        type: Date,
+        default: null,
+    },
+    requestedBy: {
+        type: String,
+        required: false,
+    },
+    track: {
+        spotifyTrackId: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        artist: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        albumArtUrl: {
+            type: String,
+            required: false,
+        },
+        spotifyLink: {
+            type: String,
+            required: true,
+        },
+        spotifyURI: {
+            type: String,
+            requried: true,
+        },
+    },
+}, {
+    timestamps: true,
+})
+
+
+requestSchema.index({ "track.spotifyTrackId": 1 });
+requestSchema.index({ roomId: 1 });
+requestSchema.index({ status: 1 });
+requestSchema.index({ roomId: 1, status: 1 });
+
+
+// TODO: === TEST THIS === \\
+// pre save hook
+requestSchema.pre("save", function (next) {
+    // if (this.status === "played" && !this.playedAt)
+    if (this.status === "played" && this.playedAt === null) {
+        this.playedAt = new Date();
+    }
+    return;
+})
+
+const Request = mongoose.model("Request", requestSchema);
+export default Request;
